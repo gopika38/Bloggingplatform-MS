c2ea9780f383e2bf6c41fd4a5a553574f80c85cc
gopika38 <148052253+gopika38@users.noreply.github.com>
2025-02-12 17:07:55 +0530

    updated

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,7 @@ const Comment = require('../models/Comment');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const { postId, content } = req.body;
+    const { postId, content } = req.body;     
     const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const comment = new Comment({ postId, userId: decoded.id, content });
